248143fa2fd47372e39f19194020e5140cfab8ca
yapie <targaryen.akane@gmail.com>
2019-09-17 17:11:36 +0700

    commit

--- Main.hs
+++ Main.hs
@@ -1,26 +1,29 @@
 import           System.Random
 import           Text.Read
 
-guessLoop :: Int -> IO ()
-guessLoop num = do
+guessLoop :: Int -> Int -> IO ()
+guessLoop _ 0 = putStrLn "Too bad you're out of life"
+guessLoop num life = do
   putStrLn "Please input your guess"
+  putStrLn $ "remain life: " ++ show life
   guessed <- getLine
   -- putStrLn (show num)
   case readMaybe guessed of
     Just a -> case compare a num of
                 GT -> do
                     putStrLn "too big"
-                    guessLoop num
+                    guessLoop num remainLife
                 LT -> do
                     putStrLn "too small"
-                    guessLoop num
+                    guessLoop num remainLife
                 EQ -> putStrLn "yay... you win"
     _ -> do
       putStrLn "oopss looks like it wasn't a number, was it?"
-      guessLoop num
+      guessLoop num life
+  where remainLife = life - 1
 
 main :: IO()
 main = do
   putStrLn "Guess the number!"
   num <- randomRIO (0, 100)
-  guessLoop num
+  guessLoop num 7
